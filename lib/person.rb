f71c74c21ecc5a3bb09502dd2686112a8d4ab317
Jose Melero Martinez <jmelero08@gmail.com>
2020-11-05 22:03:16 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -11,19 +11,3 @@ class Person
     @name 
   end 
 end 
-
-
-
-class Dog
-  def initialize(breed)
-    @breed = breed
-  end
- 
-  def breed=(breed)
-    @breed = breed
-  end
- 
-  def breed
-    @breed
-  end
-end
